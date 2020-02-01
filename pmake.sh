bdfbb35ad578e7d61fe74572d8601493ab096929
q4a <q4arus@ya.ru>
2020-02-01 08:13:01 +0300

    ci-lin 5

--- pmake.sh
+++ pmake.sh
@@ -3,4 +3,5 @@
 ### Ubuntu
 #sudo apt install libqt53drender5
 #export QT_DIR=/usr/lib/x86_64-linux-gnu
+chmod +x ./tools/premake/bin/linux/premake5
 ./tools/premake/bin/linux/premake5 --file=code/premake5.lua gmake2
